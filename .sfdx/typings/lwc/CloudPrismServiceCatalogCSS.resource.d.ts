1f5fa74aef4200e469a893506e8ca654e446e3fd
chrislmdev <cmorris0603@gmail.com>
2026-04-09 18:05:22 -0500

    Enhance CloudPrism application by adding CloudPrism_Service_Catalog tab, implementing getRosettaParentRows method in CloudPrismServiceCatalogController for mapping service catalog rows, and updating related tests. Modify permissionset to include access for CloudPrismServiceCatalogPageCtrl and ensure visibility for the new tab.

---
+++ .sfdx/typings/lwc/CloudPrismServiceCatalogCSS.resource.d.ts
@@ -0,0 +1,4 @@
+declare module "@salesforce/resourceUrl/CloudPrismServiceCatalogCSS" {
+    var CloudPrismServiceCatalogCSS: string;
+    export default CloudPrismServiceCatalogCSS;
+}
---
+++ .sfdx/typings/lwc/CloudPrismServiceCatalogJS.resource.d.ts
@@ -0,0 +1,4 @@
+declare module "@salesforce/resourceUrl/CloudPrismServiceCatalogJS" {
+    var CloudPrismServiceCatalogJS: string;
+    export default CloudPrismServiceCatalogJS;
+}
